90e3e0a63480f236bde5efb60211c1d412a9a4eb
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-16 02:34:09 +0300

    feat: download book covers

--- download_txt.py
+++ download_txt.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 import requests
 import os.path
@@ -24,7 +24,7 @@ def get_book_title(url):
     return book_title
 
 
-def get_title_and_cover(url):
+def get_book_cover(url):
     r = requests.get(url)
     r.raise_for_status()
 
@@ -32,11 +32,9 @@ def get_title_and_cover(url):
 
     soup = BeautifulSoup(r.text, 'lxml')
 
-    book_title = soup.find('title').text.split(' - ')[0]
     cover = soup.find('div', class_='bookimage').find('img')['src']
-    cover = urljoin('https://tululu.org', cover)
-    print('Заголовок:', book_title)
-    print('Автор:', cover)
+    # cover = urljoin('https://tululu.org', cover)
+    return cover
 
 
 def download_txt(url, filename, folder='books/'):
@@ -67,23 +65,47 @@ def download_txt(url, filename, folder='books/'):
             return os.path.join(folder, filename)
 
 
+def download_image(path, folder='images/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        path (str): Путь до требуемой картинки.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до картинки обложки.
+    """
+    Path(folder).mkdir(exist_ok=True)
+
+    image_name = path.split('/')[-1]
+    url = urljoin('https://tululu.org', path)
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    with open(os.path.join(folder, image_name), 'wb') as file:
+        file.write(r.content)
+        return os.path.join(folder, image_name)
+
+
 if __name__ == '__main__':
     filenames = []
+    book_covers = []
     for id in range(1, 11):
         book_url = f"https://tululu.org/b{id}/"
         try:
-            get_title_and_cover(book_url)
+            book_covers.append(get_book_cover(book_url))
+        except HTTPError:
+            print(f"Обложка книги с id{id} не найдена")
+            continue
+
+        try:
+            filenames.append(f"{id}." + get_book_title(book_url) + ".txt")
         except HTTPError:
             print(f"Книга с id{id} не найдена")
+            continue
 
-        # try:
-        #     filenames.append(f"{id}." + get_book_title(book_url) + ".txt")
-        # except HTTPError:
-        #     print(f"Книга с id{id} не найдена")
-        #     continue
+    for filename in filenames:
+        download_url = "https://tululu.org/txt.php"
+        download_txt(download_url, filename)
 
-    # for filename in filenames:
-    #     download_url = "https://tululu.org/txt.php"
-    #     download_txt(download_url, filename)
-
-
+    for book_cover in book_covers:
+        download_image(book_cover)
